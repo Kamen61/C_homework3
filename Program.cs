83fb40b5dd3bdbbfa43da02a30a0e6c517ac44f6
Kamen <canen321@gmail.com>
2022-06-30 14:28:00 +0300

    exercise3

--- Program.cs
+++ Program.cs
@@ -34,25 +34,31 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-System.Console.Write("Введите координату первой точки по x : ");
-double ax=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите координату первой точки по y : ");
-double ay=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите координату первой точки по z : ");
-double az=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите координату второй точки по x : ");
-double bx=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите координату второй точки по y : ");
-double by=Convert.ToDouble(Console.ReadLine());
-System.Console.Write("Введите координату второй точки по z : ");
-double bz=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату первой точки по x : ");
+// double ax=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату первой точки по y : ");
+// double ay=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату первой точки по z : ");
+// double az=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату второй точки по x : ");
+// double bx=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату второй точки по y : ");
+// double by=Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите координату второй точки по z : ");
+// double bz=Convert.ToDouble(Console.ReadLine());
 
-double result=(Math.Sqrt(Math.Pow(ax-bx,2)+Math.Pow(ay-by,2)+Math.Pow(az-bz,2)));
-System.Console.WriteLine("Координаты первой точки {0},{1},{2} ", ax,ay,az);
-System.Console.WriteLine("Координаты второго точки {0},{1},{2} ", bx,by,bz);
-System.Console.WriteLine(result);
+// double result=(Math.Sqrt(Math.Pow(ax-bx,2)+Math.Pow(ay-by,2)+Math.Pow(az-bz,2)));
+// System.Console.WriteLine("Координаты первой точки {0},{1},{2} ", ax,ay,az);
+// System.Console.WriteLine("Координаты второго точки {0},{1},{2} ", bx,by,bz);
+// System.Console.WriteLine(result);
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите число : ");
+byte num =Convert.ToByte(Console.ReadLine());
+for (byte i=1; i <= num; i++){
+    System.Console.WriteLine(Math.Pow(i,3));
+}
